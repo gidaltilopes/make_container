f55095f279e23747ffc3da12c81e577c491ca69d
gidalti lopes <gidaltilopes@gidaltis-MacBook-Air.local>
2018-04-04 01:18:00 -0300

    for html

--- run.py
+++ run.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+lista = ('postgres', 'nginx', 'totem')
 @app.route('/')
 def inicio():
-	return render_template('index.html', cabecalho='Meus trabalhos')
+	return render_template('index.html', cabecalho='Meus trabalhos', servicos=lista)
+
+
 
 app.run(debug=True)
